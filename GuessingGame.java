40dec0c28c6e528e68e99786e02e47bc60996f75
Abhishek Nayak <abhisheknayak25705@gmail.com>
2024-11-20 17:17:25 +0530

    Add files via upload

---
+++ GuessingGame.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.Random;
+
+class NumberGame
+{
+    public static void main(String []args)
+    {
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+        int score =0;
+        boolean TryAgain;
+        System.out.println("___Welcome To Number Game!___");
+    }
+}
